b5b3d452c4369025c8c374b3ea0277c99420f989
troyuit@gmail.com <troyuit@gmail.com@25ca8898-882d-44a0-7ab7-80c16a8908dd>
2014-10-24 09:03:50 +0000

    Finished first slide and sohoa RSS

--- ForBiz.Web/Controllers/HomeController.cs
+++ ForBiz.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity.Infrastructure;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Syndication;
@@ -8,17 +9,20 @@ using System.Xml;
 using ForBiz.Entities.Models;
 using ForBiz.Service;
 using Repository.Pattern.UnitOfWork;
+using Repository.Pattern.Infrastructure;
 
 namespace ForBiz.Web.Controllers
 {
     public class HomeController : Controller
     {
         private IQuickLinkService quickLinkService { get; set; }
+        private IArticleService articleService { get; set; }
         private IUnitOfWorkAsync unitOfWork { get; set; }
 
-        public HomeController(IQuickLinkService quickLinkService, IUnitOfWorkAsync unitOfWork)
+        public HomeController(IQuickLinkService quickLinkService, IUnitOfWorkAsync unitOfWork, IArticleService articleService)
         {
             this.quickLinkService = quickLinkService;
+            this.articleService = articleService;
             this.unitOfWork = unitOfWork;
         }
         public ActionResult Index()
@@ -26,7 +30,23 @@ namespace ForBiz.Web.Controllers
             var xmlReader = XmlReader.Create("http://vnexpress.net/rss/so-hoa.rss");
             var feed = SyndicationFeed.Load(xmlReader);
             xmlReader.Close();
-            if (feed != null) ViewBag.FeedItems = feed.Items;
+            if (feed != null) ViewBag.FeedItems = feed.Items.Take(10);
+            //foreach(var feedItem in feed.Items){
+            //    var doc = new HtmlAgilityPack.HtmlDocument();
+            //    doc.LoadHtml(feedItem.Summary.Text);
+            //    var avatar = @doc.DocumentNode.Descendants("img").Select(n => n.Attributes["src"].Value).ToArray().FirstOrDefault();
+            //    articleService.Insert(new Article
+            //    {
+            //        Author = "Số Hóa",
+            //        Avatar = avatar,
+            //        CreatedDate = DateTime.Now,
+            //        Description = feedItem.Summary.Text,
+            //        PublishedDate = DateTime.Now,
+            //        Title = feedItem.Title.Text,
+            //        ObjectState = ObjectState.Added
+            //    });
+            //}
+            //unitOfWork.SaveChangesAsync();
             return View();
         }
 
