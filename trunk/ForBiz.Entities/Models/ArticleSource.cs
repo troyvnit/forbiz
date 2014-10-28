282b8c85a2c9984388fafa9d361a1020bcc56ac1
troyuit@gmail.com <troyuit@gmail.com@25ca8898-882d-44a0-7ab7-80c16a8908dd>
2014-10-28 03:08:21 +0000

    Add BO area

---
+++ trunk/ForBiz.Entities/Models/ArticleSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.Pattern.Ef6;
+
+namespace ForBiz.Entities.Models
+{
+    public class ArticleSource : Entity
+    {
+        public int ArticleSourceId { get; set; }
+        public string ArticleSourceName { get; set; }
+        public string SourceLink { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
